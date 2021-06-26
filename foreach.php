51cfa9b1af8b09dcaf1da97d30006bbda6c77b1d
João Belarmino <joao.silva@webjump.com.br>
2021-06-26 13:22:56 -0300

    Aula 05

--- foreach.php
+++ foreach.php
@@ -25,5 +25,6 @@ $contas[38114556879] = [ // Sempre que for uma chave com significado, como um cp
 $contas[38298843894] = sacar(1.50, $contas[38298843894]);
 
 foreach($contas as $cpf => $conta){
-  echo "CPF: ". $cpf ."\nDono da conta: " . $conta['titular'] ." \nSalário: ". $conta['saldo'] . "\n\n";
+  ['titular' => $titular, 'saldo' => $saldo] = $conta;
+  echo "CPF: ". $cpf ."\nDono da conta: $titular \nSalário: $saldo\n\n";
 }
