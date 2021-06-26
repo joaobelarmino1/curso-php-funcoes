b5ea57656acc668a805b8456d4bf62716f8782c0
João Belarmino <joao.silva@webjump.com.br>
2021-06-26 12:22:12 -0300

    Aula 03

---
+++ funcoes.php
@@ -0,0 +1,39 @@
+<?php
+
+function sacar(float $valorSaque, array $conta): array 
+{
+  if($valorSaque > $conta['saldo']){
+    echo $conta['titular'] . ", você não pode sacar este valor(R$$valorSaque)!\n";
+  }else{
+    $conta['saldo'] -= $valorSaque;
+  }
+  return $conta;
+}
+
+$contas = [
+  38298843894 => [
+    'titular' => 'João',
+    'saldo' => 960
+  ], 
+  38298843893 => [
+    'titular' => 'Karen',
+    'saldo' => 1500
+  ], 
+  38288943894 => [
+    'titular' => 'Bia',
+    'saldo' => 1400
+  ]
+];
+
+//adicionando item ao array
+$contas[38114556879] = [ // Sempre que for uma chave com significado, como um cpf, é importante que ela seja mencionada, caso contrário, o php sozinho tentará incrementar como chave númerica.
+  'titular' => 'Monique',
+  'saldo' => 1420
+];
+
+$contas[38298843894] = sacar(1.50, $contas[38298843894]);
+
+foreach($contas as $cpf => $conta){
+  echo "CPF: ". $cpf ."\nDono da conta: " . $conta['titular'] ." \nSalário: ". $conta['saldo'] . "\n\n";
+}
+
